113a21fadcea8022a249a1e4a2daff8c463d391b
kc35 <g.onechawla@gmail.com>
2024-07-14 16:56:11 +0530

    Delete 2048/2048.py

--- 2048/2048.py
+++
@@ -1,7 +0,0 @@
-if __name__ == "__main__":
-   # ==== preparing main window
-   app = Play_2048()
-   app.bind_all('<Key>', app.moves)
-   app.wm_title("2048 by PythonGeeks")
-   app.minsize(430, 470)
-   app.mainloop()
