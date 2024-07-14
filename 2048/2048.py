08fd7c314d8311d1b489ed85fb3092d28e053539
kc35 <g.onechawla@gmail.com>
2024-07-14 16:30:23 +0530

    Update 2048.py

--- 2048/2048.py
+++ 2048/2048.py
@@ -1,53 +1,7 @@
-import logic
- 
-if __names__ == '__main__':
-     
-    m = logic.start_game()
- 
-while(True):
- 
-    x = input("Press the command : ")
-
-    if(x == 'W' or x == 'w'):
- 
-        m, flag_ = logic.move_up(m)
- 
-        status = logic.get_current_state(m)
-        print(status)
- 
-        if(status == 'GAME NOT OVER'):
-            logic.add_new_2(m)
- 
-        else:
-            break
- 
-    elif(x == 'S' or x == 's'):
-        m, flag_ = logic.move_down(m)
-        status = logic.get_current_state(m)
-        print(status)
-        if(status == 'GAME NOT OVER'):
-            logic.add_new_2(m)
-        else:
-            break
- 
-    elif(x == 'A' or x == 'a'):
-        m, flag_ = logic.move_left(m)
-        status = logic.get_current_state(m)
-        print(status)
-        if(status == 'GAME NOT OVER'):
-            logic.add_new_2(m)
-        else:
-            break
-
-    elif(x == 'D' or x == 'd'):
-        m, flag_ = logic.move_right(m)
-        status = logic.get_current_state(m)
-        print(status)
-        if(status == 'GAME NOT OVER'):
-            logic.add_new_2(m)
-        else:
-            break
-    else:
-        print("Invalid Key Pressed")
-      
-    print(m)
+if __name__ == "__main__":
+   # ==== preparing main window
+   app = Play_2048()
+   app.bind_all('<Key>', app.moves)
+   app.wm_title("2048 by PythonGeeks")
+   app.minsize(430, 470)
+   app.mainloop()
